22ffde6ca7cf09298b2c6749302ced60993796d5
Panda-racoon <millie3519@gmail.com>
2025-02-27 14:30:23 +0900

    프로젝트 기본 환경설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
@@ -9,5 +10,6 @@ export default defineConfig({
         plugins: ["@emotion/babel-plugin"],
       },
     }),
+    tsconfigPaths(), // 절대 경로 import 가능하게 해줌
   ],
 });
