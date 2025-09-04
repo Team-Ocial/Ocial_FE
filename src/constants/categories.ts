689f9d997c95c97df6ea5894bf9f7c46cda48d2e
Panda-racoon <millie3519@gmail.com>
2025-09-04 18:32:53 +0900

    타입에러주석처리-> 컨텐츠/카테고리

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,7 +1,9 @@
-import { ActivityCategory } from '@/types/activity.types';
+// import { ActivityCategory } from '@/types/activity.types';
 
 export const ACTIVITY_CATEGORIES = ['원데이 클래스', '스터디'] as const;
 
 export const ALL_ACTIVITIES_CATEGORY = '전체보기';
 
-export type ActivityFilterCategory = (typeof ACTIVITY_CATEGORIES)[number] | typeof ALL_ACTIVITIES_CATEGORY;
+export type ActivityFilterCategory =
+  | (typeof ACTIVITY_CATEGORIES)[number]
+  | typeof ALL_ACTIVITIES_CATEGORY;
